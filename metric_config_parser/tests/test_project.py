df5292db5704044c87bd5a25ccd9e537634ed5bc
Anna Scholtz <anna@scholtzan.net>
2022-10-28 11:19:23 -0700

    Add support for parsing metrics groups

--- metric_config_parser/tests/test_project.py
+++ metric_config_parser/tests/test_project.py
@@ -3,6 +3,7 @@ from textwrap import dedent
 import pytest
 import toml
 
+from metric_config_parser.metric import MetricReference
 from metric_config_parser.monitoring import MonitoringSpec
 
 
@@ -56,3 +57,49 @@ class TestProjectSpec:
 
         with pytest.raises(ValueError):
             MonitoringSpec.from_dict(toml.loads(config_str))
+
+    def test_metric_groups(self, config_collection):
+        config_str = dedent(
+            """
+            [project]
+            name = "foo"
+            metrics = ["foo", "bar"]
+
+            [project.metric_groups.foo]
+            friendly_name = "Fooooo"
+            metrics = [
+                "foo",
+                "bar"
+            ]
+
+            [project.population]
+            data_source = "foo"
+
+            [data_sources]
+            [data_sources.foo]
+            from_expression = "test"
+
+            [metrics]
+            [metrics.foo]
+            select_expression = "foo"
+            data_source = "foo"
+
+            [metrics.foo.statistics]
+            sum = {}
+
+            [metrics.bar]
+            select_expression = "bar"
+            data_source = "foo"
+
+            [metrics.bar.statistics]
+            sum = {}
+            """
+        )
+
+        spec = MonitoringSpec.from_dict(toml.loads(config_str))
+        config = spec.resolve(experiment=None, configs=config_collection)
+
+        assert len(config.project.metric_groups) == 1
+        assert config.project.metric_groups[0].name == "foo"
+        assert MetricReference("foo") in config.project.metric_groups[0].metrics
+        assert MetricReference("bar") in config.project.metric_groups[0].metrics
--- setup.py
+++ setup.py
@@ -63,5 +63,5 @@ setup(
         [console_scripts]
         metric-config-parser=metric_config_parser.cli:cli
     """,
-    version="2022.10.10",
+    version="2022.11.1",
 )
