03db329c08475cbae56ecf6d06f96d4950f61a44
Mike Williams <mwilliams@mozilla.com>
2023-04-27 16:40:20 -0400

    remove default False for is_enrollment_paused

--- metric_config_parser/experiment.py
+++ metric_config_parser/experiment.py
@@ -68,13 +68,13 @@ class Experiment:
     reference_branch: Optional[str]
     is_high_population: bool
     app_name: str
+    is_enrollment_paused: Optional[bool]
     app_id: Optional[str] = None
     outcomes: List[str] = attr.Factory(list)
     enrollment_end_date: Optional[dt.datetime] = None
     boolean_pref: Optional[str] = None
     channel: Optional[Channel] = None
     is_rollout: bool = False
-    is_enrollment_paused: bool = False
 
 
 @attr.s(auto_attribs=True)
