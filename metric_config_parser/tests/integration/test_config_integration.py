1f30d28a954113883343febfef7920b7ad78fbdb
Anna Scholtz <anna@scholtzan.net>
2023-05-30 14:07:13 -0700

    Add as_of() method to access config file history

--- metric_config_parser/tests/integration/test_config_integration.py
+++ metric_config_parser/tests/integration/test_config_integration.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+from pytz import UTC
+
 from metric_config_parser.config import ConfigCollection
 
 
@@ -49,3 +53,42 @@ class TestConfigIntegration:
 
         assert config_collection.configs is not None
         assert len(config_collection.definitions) > 0
+
+    def test_config_as_of(self):
+        config_collection = ConfigCollection.from_github_repo(
+            "https://github.com/mozilla/metric-hub/tree/main/jetstream"
+        ).as_of(
+            UTC.localize(datetime(2023, 5, 15))
+        )  # 6a052aea23e7e2332a20c992b2e6f07468c3d161
+
+        assert config_collection is not None
+        assert config_collection.spec_for_outcome("networking", "firefox_desktop") is None
+
+        config_collection = config_collection.as_of(UTC.localize(datetime(2023, 5, 30)))  # 0f92ef5
+        assert config_collection.spec_for_outcome("networking", "firefox_desktop") is not None
+
+    def test_config_as_of_multiple_repos(self):
+        config_collection = ConfigCollection.from_github_repos(
+            repo_urls=[
+                ConfigCollection.repo_url,
+                "https://github.com/mozilla/metric-hub/tree/main/jetstream",
+            ]
+        )
+
+        assert config_collection is not None
+        assert config_collection.spec_for_outcome("networking", "firefox_desktop") is not None
+        assert config_collection.get_metric_definition("daily_active_users_v2", "fenix") is not None
+
+        config_collection = config_collection.as_of(
+            UTC.localize(datetime(2023, 5, 15))
+        )  # 6a052aea23e7e2332a20c992b2e6f07468c3d161
+
+        assert config_collection.spec_for_outcome("networking", "firefox_desktop") is None
+        assert config_collection.get_metric_definition("daily_active_users_v2", "fenix") is None
+
+        config_collection = config_collection.as_of(
+            UTC.localize(datetime(2023, 5, 25, 20, 0, 0))  # 2fa5433
+        )
+
+        assert config_collection.spec_for_outcome("networking", "firefox_desktop") is None
+        assert config_collection.get_metric_definition("daily_active_users_v2", "fenix") is not None
