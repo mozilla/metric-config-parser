c676c4a65e1d4cf4c1e1b411fe4380e098ea98c4
Mike Williams <mwilliams@mozilla.com>
2023-10-11 15:01:35 -0400

    update test

--- metric_config_parser/tests/integration/test_config_integration.py
+++ metric_config_parser/tests/integration/test_config_integration.py
@@ -58,7 +58,7 @@ class TestConfigIntegration:
     def test_config_collection_from_branch(self):
         config_collection = ConfigCollection.from_github_repo(
             "https://github.com/mozilla/metric-hub/tree/main",
-            "https://github.com/mozilla/metric-hub/tree/main/jetstream"
+            "https://github.com/mozilla/metric-hub/tree/main/jetstream/"
         )
         assert len(config_collection.configs) > 0
 
